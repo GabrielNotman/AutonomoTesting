4c2adb1a9544816705e52c897d77f25fe875e6aa
Gabriel Notman <g.notman@gmail.com>
2015-09-09 16:26:15 +0100

    Added SleepInterrupt example

---
+++ Interrupts/SleepInterrupt/SleepInterrupt.ino
@@ -0,0 +1,48 @@
+//Note: Do not remove the start up delay.
+//After a reset, this delay gives you 
+//time to upload a new sketch before
+//the board enters sleep mode.
+
+#define INT1 4
+
+void setup() 
+{
+  //This isn't strictly necessary as default is INPUT
+  pinMode(INT1, INPUT); 
+  pinMode(13, OUTPUT);
+
+  //Attach the interrupt and set the wake flag 
+  attachInterrupt(INT1, ISR, HIGH);
+  EIC->WAKEUP.reg |= (1 << digitalPinToInterrupt(INT1));
+
+  //Set sleep mode
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+
+  //Start delay of 10s to allow for new upload after reset
+  delay(10000);
+}
+
+void ISR()
+{
+  //Blink the LED for 5s
+  digitalWrite(13, HIGH);
+  delayMicroseconds(500000);
+  digitalWrite(13, LOW);
+}
+
+void loop() 
+{
+  //Disable USB
+  USB->DEVICE.CTRLA.reg &= ~USB_CTRLA_ENABLE;
+
+  //Enter sleep mode
+  __WFI();
+  
+  //...Sleep
+  
+  //Enable USB
+  USB->DEVICE.CTRLA.reg |= USB_CTRLA_ENABLE;
+
+  //Stay awake 10s
+  delay(10000);
+}
